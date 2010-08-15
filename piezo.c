9506b3e9c828152502fa73fef40d920788881a87
Richard Barlow <richard@richardbarlow.co.uk>
2010-08-15 16:25:00 +0100

    Add a couple of functions for producing beeps/beep patterns
    These functions can be used to emit beeps at various points in the code.
    The 'piezo_beep_pattern' function can be used to indicate errors with
    the kit before any other means of outputting information is available, like
    BIOS beep codes.

--- piezo.c
+++ piezo.c
@@ -6,3 +6,9 @@ void piezo_init(void) {
 
 void piezo_play(void) {
 }
+
+void piezo_beep(void) {
+}
+
+void piezo_beep_pattern(char *pattern) {
+}
--- piezo.h
+++ piezo.h
@@ -24,4 +24,14 @@ void piezo_init(void);
  * Notes to be played will be obtained through the 'gen_note' callback */
 void piezo_play(void);
 
+/* Provides an easy way of making a 'beep' noise. Gives a 200ms beep at 1kHz
+ * and a period of silence for 100ms. Blocks until finished.*/
+void piezo_beep(void);
+
+/* Output a string of long and short beeps, 1kHz 800ms/200ms. Blocking.
+ * pattern consists of a string containing the characters '.' and '-'
+ * where '.' is a short (200ms) beep and '-' is a long (800ms) beep.
+ * A delay of 500ms is added between beeps */
+void piezo_beep_pattern(char *pattern);
+
 #endif /* __PIEZO_H */
