d6aaf33459ada0ccd6b7c15fe996abc92f01bab3
Richard Barlow <richard@richardbarlow.co.uk>
2010-08-19 18:27:21 +0100

    Greatly improve rotary encoder decoding
    There is now only a single event, on the edge of "A". As the MSP can
    only interrupt on a single edge at a time on a pin the state machine
    already knows which edge occured when poked.

--- input.c
+++ input.c
@@ -20,6 +20,8 @@
 #include "pinint.h"
 #include "leds.h"
 
+#define field_set(x, val, mask) do { x = (x & ~mask) | val; } while(0)
+
 #define BUT0 (1<<1)
 #define BUT1 (1<<3)
 #define BUT2 (1<<4)
@@ -32,61 +34,37 @@
 #define R1A (1<<5)
 #define R1B (1<<6)
 
-#define P1SIGNALS (BUT0 | BUT1 | BUT2 | RBUT1 | R1A | R1B)
-#define P2SIGNALS ((RBUT0 | R0A | R0B) >> 8)
+/* Don't include rotary encoder 'B' inputs, don't want interrupts on them */
+#define P1SIGNALS (BUT0 | BUT1 | BUT2 | RBUT1 | R1A)
+#define P2SIGNALS ((RBUT0 | R0A) >> 8)
 
-#define TEST_R_A(n) (n ? P1IN & R1A : P2IN & (R0A>>8))
 #define TEST_R_B(n) (n ? P1IN & R1B : P2IN & (R0B>>8))
 
 #define EDGE_RISING 0
 #define EDGE_FALLING 1
 /* n = rotary encoder (0 or 1)
- * inp = input pin (A or B)
  * e = edge (0 = rising, 1 = falling) */
-#define SET_R_INT_EDGE(n, inp, e) do { \
-                                  if (!n) { \
-                                      if (e) \
-                                          P2IES |=  (R0##inp >> 8); \
-                                      else \
-                                          P2IES &= ~(R0##inp >> 8); \
-                                  } else { \
-                                      if (e) \
-                                          P1IES |=  R1##inp; \
-                                      else \
-                                          P1IES &= ~R1##inp; \
-                                  } } while(0)
-
-#define EN_RB_INT(n) do { \
-                     if (!n) P2IE |= (R0B >> 8); \
-                     else    P1IE |= R1B; \
-                     } while(0)
-#define DIS_RB_INT(n) do { \
-                      if (!n) P2IE &= ~(R0B >> 8); \
-                      else    P1IE &  ~R1B; \
-                      } while(0)
-
+#define SET_RA_EDGE(n, e) do { \
+                              if (!n) \
+                                  field_set(P2IES, e ? (R0A>>8):0, (R0A>>8)); \
+                              else \
+                                  field_set(P1IES, e ? R1A:0, R1A); \
+                          } while(0)
 
 typedef enum {
-	EV_A_RISE, /* A rising  */
-	EV_A_FALL, /* A falling */
-	EV_B_RISE, /* B rising  */
-	EV_B_FALL, /* B falling */
-} input_rot_event_t;
-
-typedef enum {
-	S_AH,    /* A high, idle state */
-	S_AL_BH, /* A low, B high, anti-clockwise when entered from S_AL_BL */
-	S_AL_BL, /* Alow, B low, clockwise when entered from S_AL_BH */
+	S_IDLE, /* A high */
+	S_CW,   /* B high when entered from S_IDLE */
+	S_CCW,  /* B low, when entered from S_IDLE */
 } input_rot_state_t;
 
-static input_rot_state_t input_rot_state[2] = {S_AH, S_AH};
+static input_rot_state_t input_rot_state[2] = {S_IDLE, S_IDLE};
 
 void but_isr(uint16_t flags);
-static void input_rot_fsm(uint8_t n, input_rot_event_t ev);
+static void input_rot_fsm(uint8_t n);
 
 void input_init(void) {
 	pinint_conf[PININT_BUTTON].mask =
-	        (BUT0 | BUT1 | BUT2 | RBUT0 | RBUT1 | R0A | R0B | R1A | R1B);
+	        (BUT0 | BUT1 | BUT2 | RBUT0 | RBUT1 | R0A | R1A);
 	pinint_conf[PININT_BUTTON].int_cb = but_isr;
 
 	P1DIR &= ~P1SIGNALS; /* Set to inputs */
@@ -123,64 +101,41 @@ void but_isr(uint16_t flags) {
 	}
 
 	if (flags & R0A) {
-		if (TEST_R_A(0))
-			input_rot_fsm(0, EV_A_RISE);
-		else
-			input_rot_fsm(0, EV_A_FALL);
-	} else if (flags & R0B) {
-		if (TEST_R_B(0))
-			input_rot_fsm(0, EV_B_RISE);
-		else
-			input_rot_fsm(0, EV_B_FALL);
+		input_rot_fsm(0);
 	}
 }
 
-static void input_rot_fsm(uint8_t n, input_rot_event_t ev) {
+static void input_rot_fsm(uint8_t n) {
 	switch (input_rot_state[n]) {
-	case S_AH:
-		if (ev == EV_A_FALL && !TEST_R_B(n)) {
-			SET_R_INT_EDGE(n, A, EDGE_RISING);
-			SET_R_INT_EDGE(n, B, EDGE_RISING);
-			EN_RB_INT(n);
-			input_rot_state[n] = S_AL_BL;
-		} else if (ev == EV_A_FALL && TEST_R_B(n)) {
-			SET_R_INT_EDGE(n, A, EDGE_RISING);
-			SET_R_INT_EDGE(n, B, EDGE_FALLING);
-			EN_RB_INT(n);
-			input_rot_state[n] = S_AL_BH;
+	case S_IDLE:
+		if (TEST_R_B(n)) {
+			input_rot_state[n] = S_CW;
+		} else {
+			input_rot_state[n] = S_CCW;
 		}
+		SET_RA_EDGE(n, EDGE_RISING);
 		break;
-	case S_AL_BH:
-		if (ev == EV_B_FALL) {
-			SET_R_INT_EDGE(n, B, EDGE_RISING);
+	case S_CW:
+		if (TEST_R_B(n)) {
+			/* Must have bounced back and forth */
+			input_rot_state[n] = S_IDLE;
+		} else {
+			/* Clockwise event */
 			uled_toggle(0);
-			input_rot_state[n] = S_AL_BL;
-		} else if (ev == EV_A_RISE && TEST_R_B(n)) {
-			SET_R_INT_EDGE(n, A, EDGE_FALLING);
-			DIS_RB_INT(n);
-			input_rot_state[n] = S_AH;
-		} else if (ev == EV_A_RISE && !TEST_R_B(n)) {
-			/* Something strange happened, just return to idle */
-			SET_R_INT_EDGE(n, A, EDGE_FALLING);
-			DIS_RB_INT(n);
-			input_rot_state[n] = S_AH;
+			input_rot_state[n] = S_IDLE;
 		}
+		SET_RA_EDGE(n, EDGE_FALLING);
 		break;
-	case S_AL_BL:
-		if (ev == EV_B_RISE) {
-			SET_R_INT_EDGE(n, B, EDGE_FALLING);
+	case S_CCW:
+		if (TEST_R_B(n)) {
+			/* Count-clockwise event */
 			uled_toggle(1);
-			input_rot_state[n] = S_AL_BH;
-		} else if (ev == EV_A_RISE && !TEST_R_B(n)) {
-			SET_R_INT_EDGE(n, A, EDGE_FALLING);
-			DIS_RB_INT(n);
-			input_rot_state[n] = S_AH;
-		} else if (ev == EV_A_RISE && TEST_R_B(n)) {
-			/* Something strange happened, just return to idle */
-			SET_R_INT_EDGE(n, A, EDGE_FALLING);
-			DIS_RB_INT(n);
-			input_rot_state[n] = S_AH;
+			input_rot_state[n] = S_IDLE;
+		} else {
+			/* Again must have bounced back and forth */
+			input_rot_state[n] = S_IDLE;
 		}
+		SET_RA_EDGE(n, EDGE_FALLING);
 		break;
 	}
 }
