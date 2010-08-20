ab6e4a027ef2df8bcf36386615f7196a80a9dbed
Richard Barlow <richard@richardbarlow.co.uk>
2010-08-20 16:30:07 +0100

    Correct SCHED_TIMER to SCHED_TIMER_B in config.h
    That had me confused for a while.

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 /* Which timer module to use for the schedule module
  * 0 = TIMER_A
  * 1 = TIMER_B */
-#define SCHED_TIMER 1
+#define SCHED_TIMER_B 1
 
 /* Amount to divide SMCLK by to give a 1kHz timer */
 #define SCHED_DIV 8000
