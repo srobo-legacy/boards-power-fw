f47f47a6c8a7b6e9b2fad1102cdf46d9e9662207
Robert Spanton <rspanton@zepler.net>
2011-03-01 13:30:21 +0000

    Add anti-multi-include preprocessor stuff to note.h
    Standard.

--- note.h
+++ note.h
@@ -1,3 +1,5 @@
+#ifndef __NOTE_H
+#define __NOTE_H
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -9,3 +11,5 @@
 void note_recv_input(uint16_t flags, uint16_t edge);
 void note_enable(uint8_t enable);
 void note_poll();
+
+#endif	/* __NOTE_H */
