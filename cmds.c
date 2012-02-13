09d2ddd48a814600398f34588f04449b9b127fa7
Richard Barlow <richard@richardbarlow.co.uk>
2012-02-13 20:55:51 +0000

    Add a SRIC command to read the fw stack usage
    We might find it useful to log this. Should also help with debugging
    things like the tune playing hang-up.

--- cmds.c
+++ cmds.c
@@ -21,12 +21,14 @@
 #include "piezo.h"
 #include "power.h"
 #include "flash430/sric-flash.h"
+#include "drivers/stack.h"
 
 uint8_t sric_enable_input_notes(const sric_if_t *iface);
 uint8_t sric_play_piezo(const sric_if_t *iface);
 uint8_t sric_set_leds(const sric_if_t *iface);
 uint8_t sric_motor_rail(const sric_if_t *iface);
 uint8_t sric_get_leds(const sric_if_t *iface);
+uint8_t sric_get_stack_usage(const sric_if_t *iface);
 
 const sric_cmd_t sric_commands[] = {
 	{sric_flashr_fw_ver},
@@ -38,7 +40,8 @@ const sric_cmd_t sric_commands[] = {
 	{sric_play_piezo},
 	{sric_set_leds},
 	{sric_motor_rail},
-	{sric_get_leds}
+	{sric_get_leds},
+	{sric_get_stack_usage},
 };
 
 const uint8_t sric_cmd_num = sizeof(sric_commands) / sizeof(const sric_cmd_t);
@@ -138,4 +141,18 @@ sric_get_leds(const sric_if_t *iface)
 
 	iface->txbuf[SRIC_DATA] = resp;
 	return 1;
+}
+
+/* Takes no input, responds with 2 uint16_t with the max stack size and
+ * max stack usage */
+uint8_t
+sric_get_stack_usage(const sric_if_t *iface)
+{
+	uint16_t s_usage = stack_max_usage();
+	uint16_t s_size = stack_size;
+	iface->txbuf[SRIC_DATA+0] = s_size & 0xff;
+	iface->txbuf[SRIC_DATA+1] = s_size >> 8;
+	iface->txbuf[SRIC_DATA+2] = s_usage & 0xff;
+	iface->txbuf[SRIC_DATA+3] = s_usage >> 8;
+	return 4;
 }
